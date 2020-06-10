dfb8ae1415ee4e2a91d21e19644fd71e41a2c7ee
Ronuledge <maksimustyantsev17@gmail.com>
2020-06-10 16:39:58 +0500

    Добавьте файлы проекта.

---
+++ PasswordCheck/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Authorization
+{
+    partial class Authorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtBoxLog = new System.Windows.Forms.TextBox();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblPass.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPass.ForeColor = System.Drawing.Color.Black;
+            this.lblPass.Location = new System.Drawing.Point(114, 112);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(72, 22);
+            this.lblPass.TabIndex = 2;
+            this.lblPass.Text = "пароль";
+            // 
+            // txtBoxLog
+            // 
+            this.txtBoxLog.Location = new System.Drawing.Point(43, 61);
+            this.txtBoxLog.Name = "txtBoxLog";
+            this.txtBoxLog.Size = new System.Drawing.Size(207, 20);
+            this.txtBoxLog.TabIndex = 1;
+            this.txtBoxLog.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnDone
+            // 
+            this.btnDone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDone.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDone.ForeColor = System.Drawing.Color.Black;
+            this.btnDone.Location = new System.Drawing.Point(43, 187);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(207, 29);
+            this.btnDone.TabIndex = 3;
+            this.btnDone.Text = "Войти";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblLog.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLog.ForeColor = System.Drawing.Color.Black;
+            this.lblLog.Location = new System.Drawing.Point(114, 36);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(62, 22);
+            this.lblLog.TabIndex = 6;
+            this.lblLog.Text = "логин";
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Location = new System.Drawing.Point(43, 137);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.Size = new System.Drawing.Size(207, 20);
+            this.txtBoxPassword.TabIndex = 2;
+            this.txtBoxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(304, 255);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.txtBoxLog);
+            this.Controls.Add(this.lblPass);
+            this.ForeColor = System.Drawing.Color.DarkViolet;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Authorization";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtBoxLog;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.TextBox txtBoxPassword;
+    }
+}
+
---
+++ PasswordCheck/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Authorization
+{
+    public partial class Authorization : Form
+    {
+        string ErrorText;
+
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+        public void Check()
+        {
+           
+            char[] password = txtBoxPassword.Text.ToCharArray();
+            bool number = false;
+            bool letter = false;
+            bool symbol = false;
+
+            if (password.Length < 6)
+            {
+                ErrorText = ("Пароль должен содержать минимум 6 cимволов");
+                throw new Exception("min 6 symb");
+            }
+            else
+            {
+                for (int i = 0; i < password.Length; i++)
+                {
+                    if (password[i] >= '0' & password[i] <= '9')
+                        number = true;
+               
+                    
+                    if (password[i] >= 'A' & password[i] <= 'Z')
+                        letter = true;
+                    
+                    if (password[i] == '!' | password[i] == '@' | password[i] == '#' | password[i] == '$' | password[i] == '%' | password[i] == '^')
+                        symbol = true;
+                    
+                }
+
+
+                if (number == false)
+                {
+                    ErrorText = ("Пароль должен содержать минимум одну цифру");
+                    throw new Exception("min 1 numb");
+                }
+                if (letter == false)
+                {
+                    ErrorText = ("Пароль должен содержать минимум одну заглавную латинскую букву");
+                    throw new Exception("min 1 lett");
+                }
+                if (symbol == false)
+                {
+                    ErrorText = ("Пароль должен содержать минимум один символ из набора ! @ # $ % ^ ");
+                    throw new Exception("min 1 spec symb");
+                }
+                if (symbol == true && letter == true && number == true)
+                {
+                    StreamWriter writer = File.CreateText("users.txt");
+                    writer.WriteLine(txtBoxPassword.Text);
+                    writer.Close();
+                    MessageBox.Show("Пароль сохранен Debug/users.txt", "Готово");
+
+                }
+            } 
+       
+        }
+        public void CheckLogInPassword()
+        {
+            string Data = File.ReadAllText("users.txt");
+            if (Data.Contains(txtBoxLog.Text) == true && Data.Contains(txtBoxPassword.Text) == true)
+            { MessageBox.Show("Доступ разрешен");
+                Close();
+            }
+            else 
+            {
+                ErrorText = "Вы ввели неверный логин или пароль";
+                throw new Exception("wrong data");
+              
+            } 
+        }
+            
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+     
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CheckLogInPassword();
+            }
+
+            catch
+            {
+                MessageBox.Show(ErrorText,"Ошибка");
+            }
+        
+        }
+    }
+}
